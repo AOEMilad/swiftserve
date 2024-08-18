06e5a00a860c7db27c7111b8560cc55af1d5985a
Anthony Milad <aoemilad@gmail.com>
2024-08-18 01:41:00 -0700

    Added logo to website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 const HomePage: React.FC = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <section className="bg-blue-600 text-white w-full py-16 text-center">
+      <section className="bg-black text-white w-full py-16 text-center">
+        <img src='.\images\SwiftServe.png'></img>
         <h1 className="text-4xl font-bold mb-4">Welcome to SwiftServe</h1>
         <p className="text-lg">Your fast and reliable vending service provider.</p>
       </section>
